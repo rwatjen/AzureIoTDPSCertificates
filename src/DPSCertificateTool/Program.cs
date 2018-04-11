5bb46e08105eb368da1e78faa277efb9d9ba1b09
Rasmus Wätjen <rasw@widex.com>
2018-04-11 10:46:16 +0200

    initial commit

---
+++ src/DPSCertificateTool/Program.cs
@@ -0,0 +1,22 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+
+namespace RW.DPSCertificateTool
+{
+    [Subcommand("createdevicecert", typeof(CreateDeviceCert))]
+    [Subcommand("createcertchain", typeof(CreateCertChain))]
+    [Subcommand("createverificationcert", typeof(CreateVerificationCert))]
+    [HelpOption(ShowInHelpText = false)]
+    class Program
+    {
+        static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        public string[] RemainingArgs { get; set; }
+
+        private int OnExecute()
+        {
+            Console.Error.WriteLine("No command passed on the command line. Run with --help to see command line options.");
+            return -1;
+        }
+    }
+}
